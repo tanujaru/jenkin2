67fcb21441278f8a32d35c36aff4f065b862144b
Ritu <tanujaru@gmail.com>
2022-07-04 23:13:28 -0400

    jenkin2-1

---
+++ gitjenkin2_tanuja/src/test/java/gitjenkine2/NewTest.java
@@ -0,0 +1,26 @@
+package gitjenkine2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test
+	  public void f1() {
+		  System.out.println("hello Git jenlkin 1");
+	  }
+	  @Test
+	  public void f2() {
+		  System.out.println("hello Git jenlkin 2");
+		  System.out.println("hello Git jenlkin 3");
+	  }
+	  @Test
+	  public void f3() throws InterruptedException {
+		  System.out.println("hello Git jenlkin 2");
+		  System.setProperty("webdriver.chrome.driver", "C:\\Tanuja\\installer\\selenium\\chromedriver_win32\\chromedriver.exe");
+			WebDriver driver= new ChromeDriver();
+			driver.get("https://www.ndtv.com/");
+			Thread.sleep(3000);
+		  
+	  }
+}
