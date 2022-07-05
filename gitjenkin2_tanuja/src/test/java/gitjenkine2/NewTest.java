d3d26e6b3801d111e71f79adec3e4ab7372103fb
Ritu <tanujaru@gmail.com>
2022-07-04 23:47:21 -0400

    jenk3

--- gitjenkin2_tanuja/src/test/java/gitjenkine2/NewTest.java
+++ gitjenkin2_tanuja/src/test/java/gitjenkine2/NewTest.java
@@ -20,7 +20,7 @@ public class NewTest {
 		  System.setProperty("webdriver.chrome.driver", "C:\\Tanuja\\installer\\selenium\\chromedriver_win32\\chromedriver.exe");
 			WebDriver driver= new ChromeDriver();
 			driver.get("https://www.ndtv.com/");
-			Thread.sleep(3000);
+			Thread.sleep(2000);
 		  
 	  }
 }
